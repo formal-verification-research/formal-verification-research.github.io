215a187e7c0c95e0f5086f31413f2fc415502f7c
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2021-02-09 13:00:13 -0700

    Update Site

--- assets/scripts/toggle.js
+++ assets/scripts/toggle.js
@@ -1,8 +1,27 @@
 function toggle(name) {
-  var x = document.getElementById(name);
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  var tex = document.getElementById('c' + name);
+  var but = document.getElementById('l' + name);
+  if (tex.style.display === "none") {
+    tex.style.display = "block";
+    but.style.display = "inline";
   } else {
-    x.style.display = "none";
+    tex.style.display = "none";
+    but.style.display = "none";
   }
+}
+
+function copyBibtex(name) {
+  const copyText = document.getElementById('c' + name).textContent;
+  const textArea = document.createElement('textarea');
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  var but = document.getElementById('l' + name);
+  but.innerHTML = 'BibTeX Copied&nbsp;<i class="fas fa-check"></i>';
+  but.style.background = "#088fa3";
+  setTimeout( function() {
+    but.innerHTML = 'Copy BibTeX&nbsp;<i class="far fa-copy"></i>';
+    but.style.background = "#baebf3";},
+    1000);
 }
